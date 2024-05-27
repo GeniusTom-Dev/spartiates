0a45e7be540d843aed17916a6799a76f33d78600
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2024-05-27 16:35:45 +0200

    Modification de la page des règles du jeu en plus responsive

--- view/rules/game.php
+++ view/rules/game.php
@@ -1,12 +1,19 @@
-<h1 class="text-4xl text-gray-800 mb-2 font-serif">Bienvenue !</h1>
-<h2 class="font-serif text-3xl">Ici je vais t’apprendre les basiques du hockey sur glace.<br>
-    Alors enfile tes patins et c’est parti !</h2>
-<p class="text-3xl text-gray-600 mb-4 font-serif">Le hockey sur glace est un sport qui se pratique à <span
-            class="font-mono">6 contre 6</span>, avec <span class="font-mono">1 gardien</span> et <span
-            class="font-mono">5 joueurs</span> par équipe.<br>
-    L’objectif du jeu est de marquer un maximum de buts en envoyant un disque en caoutchouc appelé <span
-            class="font-mono">palet</span>, dans le but adverse.<br>
-    Pour manipuler le palet, les joueurs utilisent une <span class="font-mono">crosse de hockey</span>.<br>
-    Le hockey sur glace est le seul sport où les joueurs peuvent se déplacer derrière les buts !<br>
-    Le terrain de jeu, appelé patinoire, mesure entre 56 et 60 mètres de long, et entre 26 à 30 mètres de large.<br>
-    Il est entouré par des balustrades.<br></p>
+<div class="font-light bg-gray-100 min-h-screen py-4">
+    <div class="flex justify-center items-center mb-4">
+        <h1 class="bg-customBlue w-11/12 md:w-2/6 text-lg md:text-[3vh] rounded-3xl py-3 text-white mb-2 font-sans shadow-lg">Bienvenue !</h1>
+    </div>
+    <div class="px-4">
+        <h2 class="font-sans font-extralight text-base md:text-[2.5vh] mb-4 text-gray-800">Je vais t’apprendre les bases du hockey sur glace.<br>
+            Alors enfiles tes patins et c’est parti !</h2>
+    </div>
+    <div class="flex justify-center items-center mb-4">
+        <h2 class="bg-customBlue shadow-inner py-2 px-5 rounded-full text-lg md:text-xl">1</h2>
+    </div>
+    <p class="text-lg md:text-3xl text-gray-600 mb-2 font-sans px-4">
+        Le hockey sur glace est un sport qui se joue à <span class="text-customBlue">6 contre 6</span>, avec <span class="font-extralight underline">1 gardien</span> et <span class="font-extralight underline">5 joueurs</span> par équipe.<br>
+        Le but est de marquer un maximum de buts en envoyant un disque appelé <span class="text-customBlue">palet</span>, dans le but adverse.<br>
+        Pour manipuler le palet, les joueurs utilisent une <span class="text-customBlue underline">crosse de hockey</span>.<br>
+        Les joueurs peuvent se déplacer derrière les buts.<br>
+        Le terrain de jeu, appelé <span class="text-customBlue">patinoire</span>.
+    </p>
+</div>
--- view/rules/offSide.php
+++ view/rules/offSide.php
@@ -1,7 +1,18 @@
-<h1 class="text-4xl font-serif">Le hors jeu</h1>
-<p class="text-3xl font font-serif">
-    Un joueur qui attaque n’a pas le droit de précéder le palet dans la zone d’attaque.<br>
-    C’est ce qu’on appelle le hors-jeu. Autre règle très importante, celle du dégagement interdit. <br>
-    On appelle un dégagement interdit quand le palet tiré depuis la demie défensive franchit la ligne de but adverse
-    alors que les deux équipes sont à force égale ou que l’équipe qui attaque est en supériorité numérique.
-</p>
+<div class="font-light bg-gray-100 min-h-screen py-4">
+    <div class="flex justify-center items-center mb-4">
+        <h1 class="bg-customBlue w-11/12 md:w-2/6 text-lg md:text-[3vh] rounded-3xl py-3 text-white mb-2 font-sans shadow-lg">Le hors jeu</h1>
+    </div>
+    <div class="flex justify-center items-center mb-4">
+        <h2 class="bg-customBlue shadow-inner py-2 px-5 rounded-full text-lg md:text-xl">4</h2>
+    </div>
+    <p class="text-lg md:text-3xl text-gray-600 mb-4 font-sans px-4">
+        <span class="underline text-customBlue">Un joueur qui attaque</span> n’a pas le droit de précéder le palet dans la zone d’attaque.<br>
+        C’est ce qu’on appelle <span class="text-customBlue">le hors-jeu</span>. Autre règle très importante : <span class="text-customBlue">le dégagement interdit</span> (voir image ci-dessous).<br>
+    </p>
+    <div class="flex justify-center items-center mb-4 px-4">
+        <div class="bg-white shadow-lg rounded-xl p-4 w-full md:w-2/3">
+            <img src="/assets/images/degagement-interdit.jpg" alt="Illustration du dégagement interdit" class="w-full h-auto rounded-lg">
+        </div>
+    </div>
+</div>
+
