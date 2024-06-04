aae61c01d4430736a522627408a20a63e032049f
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2024-06-04 17:26:00 +0200

    Réparation bugs

--- view/forms/updateQuestion.php
+++ view/forms/updateQuestion.php
@@ -8,27 +8,59 @@
 
     <div class="w-full">
         <label>Question :
-            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="text" required><?= $data->getText() ?></textarea>
+            <textarea id="question" class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="text" required><?= $data->getText() ?></textarea>
         </label>
+        <p id="counter" class="text-[2vh] text-gray-600 text-right w-full">0/150</p>
     </div>
 
     <div class="w-full">
         <label>Bonne réponse :
-            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="true" required><?= $data->getAnswer() ?></textarea>
+            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="true" required maxlength="25"><?= $data->getAnswer() ?></textarea>
         </label>
     </div>
 
     <div class="w-full">
         <label>Mauvaise réponse 1 :
-            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="false1" required><?= $data->getFalse1() ?></textarea>
+            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="false1" required maxlength="25"><?= $data->getFalse1() ?></textarea>
         </label>
     </div>
 
     <div class="w-full">
         <label>Mauvaise réponse 2 :
-            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="false2" required><?= $data->getFalse2() ?></textarea>
+            <textarea class="w-full rounded-xl mt-2 p-2 border-gray-300" type="text" name="false2" required maxlength="25"><?= $data->getFalse2() ?></textarea>
         </label>
     </div>
 
-    <input class="bg-customBlue hover:bg-sky-300 rounded-xl text-lg py-4 px-8" type="submit" name="update" value="Mettre à jour">
+    <input id="updateQuestion" class="bg-customBlue hover:bg-sky-300 rounded-xl text-lg py-4 px-8" type="submit" name="update" value="Mettre à jour">
 </form>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const question = document.getElementById("question");
+        const counter = document.getElementById("counter");
+        const submitButton = document.getElementById("updateQuestion");
+
+
+        counter.innerHTML = question.value.length + "/150";
+        if (question.value.length >= 150) {
+            counter.style.color = "red";
+            submitButton.disabled = true;
+        } else {
+            counter.style.color = "grey";
+            submitButton.disabled = false;
+        }
+
+        question.addEventListener("input", function() {
+            const charCount = this.value.length;
+            counter.innerHTML = charCount + "/200";
+
+            if (charCount >= 200) {
+                counter.style.color = "red";
+                submitButton.disabled = true;
+            } else {
+                counter.style.color = "grey";
+                submitButton.disabled = false;
+            }
+        });
+    });
+</script>
