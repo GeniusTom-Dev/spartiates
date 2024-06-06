bb78a9c12d8997fa6da9c2fcd7105523fe102502
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2024-06-06 17:28:42 +0200

    Résolution bugs

--- view/forms/connect.php
+++ view/forms/connect.php
@@ -4,7 +4,7 @@
     <img class="p-2 bg-customBlue rounded-xl" src="../../assets/images/icon/home.svg" alt="Home">
 </a>
 
-<div class="flex justify-center items-center min-h-screen my-6">
+<div class="flex justify-center items-center min-h-screen">
         <form class="mt-2 rounded-md shadow-xl flex flex-col justify-center space-y-6 py-8 px-14 bg-white w-full max-w-2xl md:w-[95vw] sm:w-[90vw]" id="verificationForm" method="post">
        <input type="hidden" name="action" value="logIn">
 
